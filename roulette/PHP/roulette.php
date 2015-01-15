9f508d0ae7684db63af3042a49a776f0c6b02db1
Einar Belck-Olsen <ebelck@gmail.com>
2015-01-15 11:05:22 +0100

    Roulette i PHP
    Håper dette ble riktig.
    such git
    much confused

--- roulette/PHP/roulette.php
+++ roulette/PHP/roulette.php
@@ -1,5 +1,50 @@
 <?php
 
-echo "PHP roulette code goes here\n";
+function spin(){
+        return rand(0,1);
+    }
+
+    if(isset($argv[1]))
+        $spins = $argv[1];
+    else
+        $spins = 10000;
+    $rnd = spin();
+    $result = array(0,0);
+    $seq = 1;
+    $seqs = array();
+    for($i=0;$i<50;$i++)
+        array_push($seqs, NULL);
+
+    for($i=0; $i < $spins; $i++)
+    {
+        $result[$rnd]++;
+        $lastRnd = $rnd;
+        $rnd = spin();
+        if($lastRnd == $rnd)
+        {
+            $seq++;
+        }
+        else
+        {
+            if($seqs[$seq] == NULL)
+            {
+                $seqs[$seq] = 1;
+            }
+            else
+            {
+                $seqs[$seq]++;
+            }
+            $seq = 1;
+        }
+    }
+    echo "Sekvenser etter $spins spinn:\n";
+    echo "Lengde\tAntall\n";
+    foreach($seqs as $length => $count)
+    {
+        if($count > 0)
+        {
+            echo "$length\t$count\n";
+        }
+    }
 
 ?>
