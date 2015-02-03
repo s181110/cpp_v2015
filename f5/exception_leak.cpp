fb762825209bd7607e84ce57dc4c5b4f2bbf129f
Alfred Bratterud <alfred.bratterud@hioa.no>
2015-02-03 12:41:35 +0100

    Demo av exception-leak

---
+++ f5/exception_leak.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+void fill(int* arr,int size){
+  for(int i=0;i<size;i++){
+    arr[i]=42;
+    if(i>100)
+      throw string("Aw... too tired"); //throw "Aw... too tired"; (!)
+  }
+}
+  
+void alloc(){
+  const int size=1000;
+  
+  // Aldri kalle new ute delete
+  int* i=new int[size];
+  fill(i,size);
+  // Her kjøres delete - helt riktig! 
+  // (delete[]for allokering med new ..[])
+  delete[] i;
+
+  // Hvordan får vi lekkasje da?
+}
+
+/** 
+    Kode for å demonstrere en minnelekkasje forårsaket av exception
+    Kompilér med "make exception_leak" 
+    Kjør med "valgrind ./exception_leak". 
+ **/
+int main(){
+  try{
+    alloc();
+  }catch(string s){
+    cout << "Exception caught: "<< s << endl;
+  }
+ 
+  // Programmet fortsetter i en ... meny, event-loop el.
+  string input;
+  while(true){
+    cin >> input;
+    cout << input;
+  }
+
+  // Lekkasjer?
+}
