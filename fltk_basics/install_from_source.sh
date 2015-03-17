20a351027186e5e0db04908a755c86d949e46719
Alfred Bratterud <alfred.bratterud@hioa.no>
2015-03-17 13:46:50 +0100

    Installasjonsscript for FLTK

---
+++ fltk_basics/install_from_source.sh
@@ -0,0 +1,28 @@
+DOWNLOADDIR=~
+cd $DOWNLOADDIR
+
+echo 
+echo "==> Downloading FLTK to " $DOWNLOADDIR 
+echo 
+wget http://fltk.org/pub/fltk/1.3.3/fltk-1.3.3-source.tar.gz 
+
+echo 
+echo "==> Extracting FLTK"
+echo 
+tar -xvf fltk-1.3.3-source.tar.gz
+cd fltk-1.3.3
+
+echo 
+echo "==> Configuring FLTK"
+echo 
+./configure
+
+echo 
+echo "==> Building FLTK"
+echo 
+make
+
+echo 
+echo "==> Installing FLTK"
+echo 
+sudo make install
